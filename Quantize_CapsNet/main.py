f2c7d95984a89a223b38077ef8eb4cc16d9b6474
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2018-07-05 15:01:33 +0530

    Update

--- Quantize_CapsNet/main.py
+++ Quantize_CapsNet/main.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from config import cfg
 from utils import load_data
 from capsNet import CapsNet
+from convNet import ConvNet
 
 
 def save_to():
@@ -16,14 +17,14 @@ def save_to():
         loss = cfg.results + '/loss.csv'
         train_acc = cfg.results + '/train_acc.csv'
         val_acc = cfg.results + '/val_acc.csv'
-        
+
         if os.path.exists(val_acc):
             os.remove(val_acc)
         if os.path.exists(loss):
             os.remove(loss)
         if os.path.exists(train_acc):
             os.remove(train_acc)
-        
+
         fd_train_acc = open(train_acc, 'w')
         fd_train_acc.write('step,train_acc\n')
         fd_loss = open(loss, 'w')
@@ -43,7 +44,7 @@ def save_to():
 def train(model, supervisor, num_label):
     trX, trY, num_tr_batch, valX, valY, num_val_batch = load_data(cfg.dataset, cfg.batch_size, is_training=True)
     Y = valY[:num_val_batch * cfg.batch_size].reshape((-1, 1))
-    
+
     fd_train_acc, fd_loss, fd_val_acc = save_to()
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -58,19 +59,19 @@ def train(model, supervisor, num_label):
                 start = step * cfg.batch_size
                 end = start + cfg.batch_size
                 global_step = epoch * num_tr_batch + step
-                
+
                 if global_step % cfg.train_sum_freq == 0:
                     _, loss, train_acc, summary_str = sess.run([model.train_op, model.total_loss, model.accuracy, model.train_summary])
                     assert not np.isnan(loss), 'Something wrong! loss is nan...'
                     supervisor.summary_writer.add_summary(summary_str, global_step)
-                    
+
                     fd_loss.write(str(global_step) + ',' + str(loss) + "\n")
                     fd_loss.flush()
                     fd_train_acc.write(str(global_step) + ',' + str(train_acc / cfg.batch_size) + "\n")
                     fd_train_acc.flush()
                 else:
                     sess.run(model.train_op)
-                
+
                 if cfg.val_sum_freq != 0 and (global_step) % cfg.val_sum_freq == 0:
                     val_acc = 0
                     for i in range(num_val_batch):
@@ -81,7 +82,7 @@ def train(model, supervisor, num_label):
                     val_acc = val_acc / (cfg.batch_size * num_val_batch)
                     fd_val_acc.write(str(global_step) + ',' + str(val_acc) + '\n')
                     fd_val_acc.flush()
-            
+
             if (epoch + 1) % cfg.save_freq == 0:
                 supervisor.saver.save(sess, cfg.logdir + '/model_epoch_%04d_step_%02d' % (epoch, global_step))
 
@@ -96,7 +97,7 @@ def evaluation(model, supervisor, num_label):
     with supervisor.managed_session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         supervisor.saver.restore(sess, tf.train.latest_checkpoint(cfg.logdir))
         tf.logging.info('Model restored!')
-        
+
         test_acc = 0
         for i in tqdm(range(num_te_batch), total=num_te_batch, ncols=70, leave=False, unit='b'):
             start = i * cfg.batch_size
@@ -112,11 +113,11 @@ def evaluation(model, supervisor, num_label):
 def main(_):
     tf.logging.info(' Loading Graph...')
     num_label = 10
-    model = CapsNet()
+    model = ConvNet()
     tf.logging.info(' Graph loaded')
-    
+
     sv = tf.train.Supervisor(graph=model.graph, logdir=cfg.logdir, save_model_secs=0)
-    
+
     if cfg.is_training:
         tf.logging.info(' Start training...')
         train(model, sv, num_label)
